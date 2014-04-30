79a6258b6ac98d8140745e01d32d59e3549e966f
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-04-30 03:14:30 -0300

    More robust end-of-process detection.

--- SimployCommandRunner.java
+++ SimployCommandRunner.java
@@ -4,15 +4,40 @@ import java.io.InputStream;
 
 public class SimployCommandRunner {
 
+	@SuppressWarnings("deprecation")
 	static String exec(String command) throws Exception {
-		Process process = startProcess(command);
-		String stdOut = capture(process.getInputStream());
+		final Process process = startProcess(command);
+		
+		final StringBuilder stdout = new StringBuilder();
+		Thread capturing = new Thread() { public void run() {
+			try {
+				capture(process.getInputStream(), stdout);
+			} catch (ThreadDeath td) {
+				//Reading from the inputStream above will block sometimes when the process ends. :(
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}};
+		capturing.start();
 
-		if (process.waitFor() != 0)
+		waitForExit(process);
+		sleepASecond();
+		capturing.stop(new ThreadDeath());
+
+		if (process.exitValue() != 0)
 			throw new Exception("Command failed: " + command);
 
 		System.out.println("\n");
-		return stdOut;
+		return stdout.toString();
+	}
+
+
+	private static void sleepASecond() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 
@@ -26,13 +51,22 @@ public class SimployCommandRunner {
 	}
 
 
-	private static String capture(InputStream inputStream) throws Exception {
-		String result = "";
+	private static void capture(InputStream inputStream, StringBuilder stdout) throws Exception {
 		while (true) {
-			int read = inputStream.read();
-			if (read == -1) return result;
-			result += (char)read;
+			int read = inputStream.read(); //This will block forever sometimes when the process ends. :(
+			if (read == -1)
+				return;
+			stdout.append((char)read);
 			System.out.print((char)read);
+		}
+	}
+
+
+	private static void waitForExit(Process process) {
+		try {
+			process.waitFor();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
